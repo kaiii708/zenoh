7fa036051dc29ca7d73363f82813e7851157fcd9
kaiii708 <yuvivian0708@gmail.com>
2026-06-19 17:11:31 +0800

    Disable packet count instrumentation logging in face.rs

--- zenoh/src/net/routing/dispatcher/face.rs
+++ zenoh/src/net/routing/dispatcher/face.rs
@@ -326,9 +326,8 @@ impl Primitives for Face {
                 unregister_expr(&self.tables, &mut self.state.clone(), m.id);
             }
             zenoh_protocol::network::DeclareBody::DeclareSubscriber(m) => {
-                let ts = std::time::SystemTime::now()
-                    .duration_since(std::time::UNIX_EPOCH).unwrap().as_micros();
-                eprintln!("[PACKET_COUNT] DeclareSubscriber received timestamp_us={ts}");
+                // let now = std::time::SystemTime::now();
+                // eprintln!("[{}] [PACKET_COUNT] DeclareSubscriber received", humantime::format_rfc3339_micros(now));
                 let mut declares = vec![];
                 declare_subscription(
                     ctrl_lock.as_ref(),
@@ -346,9 +345,8 @@ impl Primitives for Face {
                 }
             }
             zenoh_protocol::network::DeclareBody::DeclarePreSubscriber(m) => {
-                let ts = std::time::SystemTime::now()
-                    .duration_since(std::time::UNIX_EPOCH).unwrap().as_micros();
-                eprintln!("[PACKET_COUNT] DeclarePreSubscriber received timestamp_us={ts}");
+                // let now = std::time::SystemTime::now();
+                // eprintln!("[{}] [PACKET_COUNT] DeclarePreSubscriber received", humantime::format_rfc3339_micros(now));
                 let mut declares = vec![];
                 declare_presubscription(
                     ctrl_lock.as_ref(),
@@ -369,9 +367,8 @@ impl Primitives for Face {
                 }
             }
             zenoh_protocol::network::DeclareBody::DeclareRouteUpdate(m) => {
-                let ts = std::time::SystemTime::now()
-                    .duration_since(std::time::UNIX_EPOCH).unwrap().as_micros();
-                eprintln!("[PACKET_COUNT] DeclareRouteUpdate received timestamp_us={ts}");
+                // let now = std::time::SystemTime::now();
+                // eprintln!("[{}] [PACKET_COUNT] DeclareRouteUpdate received", humantime::format_rfc3339_micros(now));
                 let mut declares = vec![];
                 declare_routeupdate(
                     ctrl_lock.as_ref(),
